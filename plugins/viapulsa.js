73764e45a381b087f5b62fc8b7dc8f6bbb98a622
DanzGantevk75 <102861930+DanzGantevk75@users.noreply.github.com>
2022-04-05 23:26:22 +0700

    Update viapulsa.js

--- plugins/viapulsa.js
+++ plugins/viapulsa.js
@@ -1,9 +1,9 @@
 let handler  = async (m, { conn, usedPrefix: _p }) => {
 let info = `╠═〘 PEMBAYARAN 〙 ═
 ╠═ Untuk Pembayaran Via Umobile
-╠➥ *60189830350*
+╠➥ *6288270863279*
 ╠➥ Chat Owner:
-║- wa.me/60189830350
+║- wa.me/6288270863279
 ╠═〘 LYNXZY 〙 ═
 `.trim()
 
